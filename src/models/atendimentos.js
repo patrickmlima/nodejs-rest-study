8edc730407aaf0ce7cf46a4e1a2e8651cbfa50df
Patrick Lima <patrickm.lima1@gmail.com>
2021-03-11 01:43:00 -0400

    feat: Validating data

--- src/models/atendimentos.js
+++ src/models/atendimentos.js
@@ -9,6 +9,24 @@ class Atendimento {
 
         const isValidDate = moment(data).isSameOrAfter(created);
         const isClientValid = atendimento.cliente.length >= 5;
+
+        const validation = [
+            {
+                field: 'data',
+                isValid: isValidDate,
+                message: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                field: 'cliente',
+                isValid: isClientValid,
+                message: 'Nome do cliente deve ter pelo menos 5 caracteres'
+            }
+        ]
+
+        const errors = validation.filter(v => !v.isValid);
+        if (errors.length > 0) {
+            return res.status(400).json(errors);
+        }
         
         const atendimentoDatado = { ...atendimento, data_criacao: created, data};
 
